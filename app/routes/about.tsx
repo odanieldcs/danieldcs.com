f153a24412ef7e5e1605ce5740c8ddef74fc69e5
Daniel Castro <2968031+odanieldcs@users.noreply.github.com>
2025-05-31 15:02:03 -0300

    feat: add about page and update routing configuration

---
+++ app/routes/about.tsx
@@ -0,0 +1,9 @@
+import About from '@content/about.mdx'
+
+export default function AboutPage() {
+  return (
+    <div className="prose prose-invert mx-auto max-w-3xl px-4 py-8">
+      <About />
+    </div>
+  )
+}
