fb89b3c8998686ee3a76dcf2bf74344c22ae7da5
Daniel Castro <2968031+odanieldcs@users.noreply.github.com>
2025-05-31 15:36:42 -0300

    chore(tests): add initial setup using Vitest, RTL, TypeScript

---
+++ tests/setup/helpers.ts
@@ -0,0 +1,4 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import { describe, expect, it, vi } from 'vitest'
+
+export { describe, it, expect, render, screen, vi, fireEvent }
